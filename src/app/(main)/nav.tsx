63b73582968bbcd31b5d332e3c7cf7c343729543
Euan Morgan <euanmorgan48@gmail.com>
2024-09-11 23:03:44 +0100

    fix: shadcn scrollbar of doom

--- src/app/(main)/nav.tsx
+++ src/app/(main)/nav.tsx
@@ -20,7 +20,7 @@ const navItems = [
 export function Nav() {
   const pathname = usePathname();
   return (
-    <header className="fixed left-0 right-0 top-0 z-50 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+    <header className="global-navbar fixed left-0 right-0 top-0 z-50 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
       <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
         <Link
           href="#"
